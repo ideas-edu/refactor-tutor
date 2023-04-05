e96d9bc1f914dd886e34408f8962c81b0c28ec95
Stephan Stanisic <s.stanisic@uu.nl>
2023-04-05 14:05:17 +0200

    Add buggy rule `count = count + 1` ~> `count =+ 1`

--- src/Domain/Refactoring/Exercise.hs
+++ src/Domain/Refactoring/Exercise.hs
@@ -52,7 +52,7 @@ makeRefactorExercise refExInput settings = addReady $ emptyExercise
         canBeRestarted = true,
         examples       = examplesWithDifficulty [(Medium, startCM refExInput)],
         constraints    = map c2 (testCases refExInput), -- check in diagnose
-        extraRules     = [use buggyEqualsTrue, use buggyCollapseIfR, use incrementAssignBuggy, use compoundSubtractionBuggy, use decrementAssignBuggy]
+        extraRules     = [use buggyEqualsTrue, use buggyCollapseIfR, use incrementAssignBuggy, use compoundSubtractionBuggy, use decrementAssignBuggy, use compoundAdditionBuggy]
       }
     where
         addReady e = e { ready = predicate (stepsLeft e) }

10dd3aef1da3e8219e0fd02ac281ce3e3d80331f
Stephan Stanisic <s.stanisic@uu.nl>
2023-06-20 11:57:25 +0200

    Switch builds from stack to cabal

--- .docker/setup.sh
+++ .docker/setup.sh
@@ -2,38 +2,23 @@
 
 # Make sure we run noninteractive and skip tzdata config
 export DEBIAN_FRONTEND=noninteractive
-ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+sudo ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 
 # Install GHCup dependencies
-apt update
-apt install -y libsqlite3-dev zlib1g-dev wget pkg-config g++ gcc \
-               libc6-dev libffi-dev libgmp-dev make xz-utils zlib1g-dev \
-               git gnupg netbase curl build-essential libgmp10 \
-               libncurses-dev libncurses5 libtinfo5 sudo
+sudo apt update
+sudo apt install -y libsqlite3-dev zlib1g-dev wget pkg-config g++ gcc \
+                    libc6-dev libffi-dev libgmp-dev make xz-utils zlib1g-dev \
+                    git gnupg netbase curl build-essential libgmp10 \
+                    libncurses-dev libncurses5 libtinfo5 sudo
 
 # Configure GHCup
 export BOOTSTRAP_HASKELL_GHC_VERSION=8.6.5
 export BOOTSTRAP_HASKELL_NONINTERACTIVE=true
 export BOOTSTRAP_HASKELL_ADJUST_BASHRC=true
 
-
-# Make sure we have an user with uid 100
-USER_ID=1000
-id $USER_ID
-if [ $? -ne 0 ]; then
-  useradd -u $USER_ID -m user
-fi
-
-# Help sudo -E along a little
-export USER="$(id -un $USER_ID)"
-export HOME="$(eval echo ~$USER)"
-export SHELL=/bin/bash
-export TMPDIR=/tmp
-cd $HOME
-
 export GHCUP_INSTALL_BASE_PREFIX=/opt/ghcup
-mkdir -p $GHCUP_INSTALL_BASE_PREFIX
-chown -R $USER:$USER $GHCUP_INSTALL_BASE_PREFIX
+sudo mkdir -p $GHCUP_INSTALL_BASE_PREFIX
+sudo chown -R $USER:$USER $GHCUP_INSTALL_BASE_PREFIX
 
 # Install GHCup
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sudo -Eu $USER sh
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org |  sh
